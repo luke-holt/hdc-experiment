a1134ca59ba0deeff258447b24b7bbcd491ec161
Luke Holt <luke@lukeholt.dev>
2025-03-09 22:57:41 -0400

    remove symbolcount

--- build.sh
+++ build.sh
@@ -3,7 +3,4 @@
 mkdir -p bin
 
 gcc -o bin/vector -Iinc src/hdvector.c src/vector.c
-
-gcc -o bin/symbolcount -Iinc src/symbolcount.c
-
 gcc -o bin/testsuite -Iinc -Itest src/hdvector.c test/test_hdvector.c test/test.c
